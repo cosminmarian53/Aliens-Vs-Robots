f47b129f08c80c58fe6749dedb665ce82ed6420e
cosminmarian53 <cosmin.lavric@student.unitbv.ro>
2024-10-10 12:07:32 +0300

    Added foundry project

---
+++ ethereum-backend/src/AvRNFTMinter.sol
@@ -0,0 +1,52 @@
+// Layout of the contract file:
+// version
+// imports
+// errors
+// interfaces, libraries, contract
+// Inside Contract:
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+
+// external & public view & pure functions
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.27;
+
+/**
+ * @title AvRSNFTMinter
+ * @author cosminmarian53
+ * @dev AvRSNFTMinter contract is used to mint NFTs for AvR(Aliens vs Robots)
+ * @notice This contract is used to mint NFTs for AvR(Aliens vs Robots)
+ */
+
+// Contract implementation
+contract AvRSNFTMinter {
+    // Type declarations
+    // State variables
+    // Events
+    // Modifiers
+    // <-------Functions layout------->
+    // constructor
+    // receive function (if exists)
+    // fallback function (if exists)
+    // external
+    // public
+    // internal
+    // private
+    // internal & private view & pure functions
+    function mintNFTs() public {
+        // Mint NFT
+    }
+}
